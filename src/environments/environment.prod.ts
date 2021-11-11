9e0e550ea2034abaa44ea0d4411938d8d03b358f
avilapro <mejiasavilaeduardo@gmail.com>
2021-11-11 12:38:18 -0400

    actualizando y setting para firebase 001

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'technodevs',
+    appId: '1:499234238369:web:6d882b258b8b3b02a9a5e0',
+    storageBucket: 'technodevs.appspot.com',
+    apiKey: 'AIzaSyABEPBn4LGvpi9sVG_AjuyjlJI5pY3P1g8',
+    authDomain: 'technodevs.firebaseapp.com',
+    messagingSenderId: '499234238369',
+    measurementId: 'G-94SCECLFVZ',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,25 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'technodevs',
+    appId: '1:499234238369:web:6d882b258b8b3b02a9a5e0',
+    storageBucket: 'technodevs.appspot.com',
+    apiKey: 'AIzaSyABEPBn4LGvpi9sVG_AjuyjlJI5pY3P1g8',
+    authDomain: 'technodevs.firebaseapp.com',
+    messagingSenderId: '499234238369',
+    measurementId: 'G-94SCECLFVZ',
+  },
+  production: false,
+  firebae: {
+    apiKey: "AIzaSyABEPBn4LGvpi9sVG_AjuyjlJI5pY3P1g8",
+    authDomain: "technodevs.firebaseapp.com",
+    projectId: "technodevs",
+    storageBucket: "technodevs.appspot.com",
+    messagingSenderId: "499234238369",
+    appId: "1:499234238369:web:6d882b258b8b3b02a9a5e0",
+    measurementId: "G-94SCECLFVZ"
+  }
 };
 
 /*
